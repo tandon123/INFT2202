f9f5f964e36f81beec50595d25144d9174094032
tandon123 <tandonsaksham0212@gmail.com>
2024-01-25 15:00:50 -0500

    ICE1

--- ICE1-Starter/ICE1-Starter/list.js
+++ ICE1-Starter/ICE1-Starter/list.js
@@ -1,31 +1,31 @@
+let list = document.getElementById("theList")
 // ADDING ITEMS TO START AND END OF LIST
+let element = document.createElement("li");
 // Get the <ul> element
-
+let text = document.createTextNode("cream");
 // ADD NEW ITEM TO END OF LIST
-// Create element
-// Create text node
-// Add text node to element
-// Add element end of list
+element.appendChild(text);
+list.appendChild(element);
 
-// ADD NEW ITEM START OF LIST
-// Create element
-// Create text node
-// Add text node to element
-// Add element to list
+element = document.createElement("li");
 
+text = document.createTextNode("spinach");
 
+element.appendChild(text);
+
+// ADD NEW ITEM TO START OF LIST
+list.insertBefore(element, list.firstChild);
 
 // All <li> elements
+let allListItems = document.getElementsByTagName("li");
 
 // ADD A CLASS OF COOL TO ALL LIST ITEMS
-// Counter variable
-// Loop through elements
-// Change class to cool
-
+for (let i = 0; i < allListItems.length; i++) {
+    allListItems[i].classList.add("cool");
+}
 
 // ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
-// h2 element
-// h2 text
-// No. of <li> elements
-// Content
-// Update h2 using innerHTML (not textContent) because it contains markup
+let h2 = document.createElement("h2");
+let itemCountText = document.createTextNode("Number of Items: " + list.children.length);
+h2.appendChild(itemCountText);
+document.body.insertBefore(h2, list);
