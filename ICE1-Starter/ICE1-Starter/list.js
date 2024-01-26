f9f4eca521a4b621f033ceb9cb50a266c306fca2
tandon123 <tandonsaksham0212@gmail.com>
2024-01-26 03:14:27 -0500

    ICE1

--- ICE1-Starter/ICE1-Starter/list.js
+++ ICE1-Starter/ICE1-Starter/list.js
@@ -1,31 +1,51 @@
-let list = document.getElementById("theList")
-// ADDING ITEMS TO START AND END OF LIST
+//Name: Saksham Tandon
+//File: list.js
+//Description: We are adding 2 new items to the given list and making it worth it.
+//Date: 25th jan 2024
+
+// Get the <ul> element with id "theList"
+let list = document.getElementById("theList");
+
+// Create a new list item element
 let element = document.createElement("li");
-// Get the <ul> element
+
+// Create a text node with the content "cream"
 let text = document.createTextNode("cream");
-// ADD NEW ITEM TO END OF LIST
+
+// Add the text node to the list item element
 element.appendChild(text);
+
+// Add the list item element to the end of the list
 list.appendChild(element);
 
+// Create another list item element
 element = document.createElement("li");
 
+// Create a text node with the content "spinach"
 text = document.createTextNode("spinach");
 
+// Add the text node to the second list item element
 element.appendChild(text);
 
-// ADD NEW ITEM TO START OF LIST
+// Add the second list item element to the start of the list
 list.insertBefore(element, list.firstChild);
 
-// All <li> elements
+// Get all <li> elements in the document
 let allListItems = document.getElementsByTagName("li");
 
-// ADD A CLASS OF COOL TO ALL LIST ITEMS
+// Add a class "cool" to all <li> elements
 for (let i = 0; i < allListItems.length; i++) {
     allListItems[i].classList.add("cool");
 }
 
-// ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
+// Create an <h2> element
 let h2 = document.createElement("h2");
+
+// Create a text node with the content "Number of Items: " + number of child elements in the list
 let itemCountText = document.createTextNode("Number of Items: " + list.children.length);
+
+// Add the text node to the <h2> element
 h2.appendChild(itemCountText);
+
+// Insert the <h2> element before the list in the body of the document
 document.body.insertBefore(h2, list);
